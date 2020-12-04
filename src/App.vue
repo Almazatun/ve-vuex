ee122074dfd4228e215360293522ed2760d3a421
Almaz <atunstyle@gmail.com>
2020-12-05 00:05:49 +0300

    add Routes component and :recycle:refactor

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/todolists">Todo-lists</router-link>
-  </div>
-  <router-view/>
+<template id="app">
+  <Routes/>
 </template>
 
 <script>
+import Routes from "@/components/Routes";
+
 export default {
   name: 'App',
+  components: {
+    Routes: Routes
+  }
 }
 </script>
 
@@ -20,18 +20,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
-import Todolists  from "@/views/Todolists";
+import Todolists  from "@/views/TodoLists";
 
 const PATH = {
   home: '/',
