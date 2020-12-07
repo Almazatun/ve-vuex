17aed7bffb9a9394aa1b490aab90e6b15588be0a
Almaz <atunstyle@gmail.com>
2020-12-07 21:14:00 +0300

    :recycle:refactor *delete About page

--- src/components/Routes.vue
+++ src/components/Routes.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
+    <router-link to="/">Home</router-link>
+    |
     <router-link to="/todolists">Todo-lists</router-link>
   </div>
   <router-view/>
@@ -9,7 +9,7 @@
 
 <script>
 export default {
-name: "Routes"
+  name: "Routes",
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
-import Todolists  from "@/views/TodoLists";
+import {createRouter, createWebHashHistory} from 'vue-router'
+import TodoLists from "../views/TodoLists";
 
 const PATH = {
   home: '/',
@@ -12,20 +11,12 @@ const routes = [
   {
     path: PATH.home,
     name: 'Home',
-    component: Home
-  },
-  {
-    path: PATH.about,
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/Home.vue')
   },
   {
     path: PATH.todoLists,
     name: 'Todolists',
-    component: Todolists
+    component: TodoLists
   },
 ]
 
